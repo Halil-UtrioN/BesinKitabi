d865d92f3494020bf881d82e6021d5b1eaf0841b
Halil İbrahim ÇAKIR <hibrahim54@cakirhalil.com>
2025-01-04 13:00:05 +0300

    Fragments were created.

---
+++ app/src/main/java/com/cakirhalil/besinkitabi/fragment/BesinListeFragment.kt
@@ -0,0 +1,24 @@
+package com.cakirhalil.besinkitabi.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.cakirhalil.besinkitabi.R
+
+
+class BesinListeFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_besin_liste, container, false)
+    }
+}
